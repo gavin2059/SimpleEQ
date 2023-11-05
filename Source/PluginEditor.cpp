2a6aba42d7e3d492b87ea8681d8954cad17cecc4
gavin2059 <zz66@rice.edu>
2023-11-05 16:27:57 -0600

    square sliders

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -53,7 +53,15 @@ void RotarySliderWithLabels::paint(juce::Graphics &g)
 
 juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
 {
-    return getLocalBounds();
+    auto bounds = getLocalBounds();
+    auto size = juce::jmin(bounds.getWidth(), bounds.getHeight());
+    size -= getTextHeight() * 2;
+    juce::Rectangle<int> r;
+    r.setSize(size, size);
+    r.setCentre(bounds.getCentreX(), 0);
+    r.setY(2);
+    
+    return r;
 }
 
 //===========================================================================================
