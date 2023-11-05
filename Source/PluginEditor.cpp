a811ac7dcdbc9a24a05df8a8cacb89779bdd1e85
gavin2059 <zz66@rice.edu>
2023-11-05 17:58:29 -0600

    added axes

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -235,8 +235,8 @@ void ResponseCurveComponent::resized()
     Graphics g(background);
     Array<float> freqs
     {
-        20,30,40,50,100,200,300,400,500,1000,
-        2000,3000,4000,5000,10000,20000
+        20,50,100,200,500,1000,
+        2000,5000,10000,20000
     };
     
     auto renderArea = getAnalysisArea();
@@ -268,7 +268,49 @@ void ResponseCurveComponent::resized()
         g.setColour(gDb != 0.f ? Colours::darkgrey : Colour(0u,172u,1u));
         g.drawHorizontalLine(y, left, right);
     }
-    
+   
+    g.setColour(Colours::lightgrey);
+    const int fontHeight = 10;
+    g.setFont(fontHeight);
+    for (int i = 0; i < freqs.size(); i++)
+    {
+        auto f = freqs[i];
+        auto x = xs[i];
+        bool addK = false;
+        String str;
+        if (f > 999.f)
+        {
+            addK = true;
+            f /= 1000.f;
+        }
+        str << f;
+        if(addK)
+            str << "k";
+        str << "Hz";
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setCentre(x, 0);
+        r.setY(1);
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+    }
+    for (auto gDb: gain)
+    {
+        auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
+        String str;
+        if (gDb > 0)
+            str << "+";
+        str << gDb;
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setX(getWidth() - textWidth);
+        r.setCentre(r.getCentreX(), y);
+        g.setColour(gDb == 0.f ? Colour(0u,172u,1u) : Colours::lightgrey);
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+        
+        
+    }
 }
 
 juce::Rectangle<int> ResponseCurveComponent::getRenderArea()
